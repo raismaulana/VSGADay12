22257c24ce609d67224fc974898dd5f0100c6749
Lufri Rais Maulana <lufriraismaulana@gmail.com>
2020-10-14 16:48:20 +0700

    fixing bug

--- app/src/main/java/com/lufriraismaulana/latihanmaps/MapsActivity.java
+++ app/src/main/java/com/lufriraismaulana/latihanmaps/MapsActivity.java
@@ -1,27 +1,177 @@
 package com.lufriraismaulana.latihanmaps;
 
-import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, View.OnClickListener {
 
     private GoogleMap mMap;
+    double latitude;
+    double longitude;
+    Button btnKoordinat;
+    Button btnPosisiUser;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 666;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        btnKoordinat = findViewById(R.id.btn_koordinat);
+        btnPosisiUser = findViewById(R.id.btn_posisi_user);
+
+        btnKoordinat.setOnClickListener(this);
+        btnPosisiUser.setOnClickListener(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        checkLocationPermission();
+        cekGPS();
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         mapFragment.getMapAsync(this);
+        if (latitude != 0 && longitude != 0) {
+            Toast.makeText(this, "Latitude : " + latitude + ", Longitude: " + longitude, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Peringatan")
+                        .setMessage("Perizinan lokasi")
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void cekGPS() {
+        // cek GPS hidup atau tidak
+        Log.d("sa", "cekGPS: bagian atas");
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Log.d("sa", "cekGPS: bagian atas kedua");
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("sa", "cekGPS: !manager.isProviderEnabled(LocationManager.GPS_PROVIDER)");
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Info");
+            builder.setMessage("Anda akan mengaktifkan GPS ?");
+            builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.create().show();
+        }
+        Log.d("sa", "cekGPS: setelah if");
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+        //menampilkan status google play service
+        if (status != ConnectionResult.SUCCESS) {
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
+            Log.d("TAG", "cekGPS: ConnectionResult.SUCCESS gagal");
+        } else {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            Log.d("sa", "cekGPS: ConnectionResult.SUCCESS masuk else");
+            //membuat kriteria untuk penumpangan provider
+            Criteria criteria = new Criteria();
+
+            //mencari provider terbaik
+            String provider = locationManager.getBestProvider(criteria, true);
+
+            //mendapatkan lokasi terakhir
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+
+                return;
+            }
+            Log.d("asa", "cekGPS: berarti tidak masuk sini");
+            Location location = locationManager.getLastKnownLocation(provider);
+            Log.d("sadas", "cekGPS: " + location);
+            if (location != null) {
+                onLocationChanged(location);
+                Log.d("s", "cekGPS: lokasi tidak null");
+            } else {
+                Log.d("s", "cekGPS: lokasi null");
+            }
+            locationManager.requestLocationUpdates(provider, 5000, 0, this);
+        }
     }
 
     /**
@@ -37,9 +187,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (checkLocationPermission()) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("MainActivity.java", "onLocationChanged: dipanggil");
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (latitude != 0 && longitude != 0) {
+            if (view == btnKoordinat) {
+                Toast.makeText(MapsActivity.this, "Latitude : " + latitude + ", Longitude: " + longitude, Toast.LENGTH_SHORT).show();
+            } else if (view == btnPosisiUser) {
+                LatLng user = new LatLng(latitude, longitude);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(user, 12));
+
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //PERMISSION didapatkan
+                    Criteria criteria = new Criteria();
+                    String provider = locationManager.getBestProvider(criteria, true);
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                        cekGPS();
+                        locationManager.requestLocationUpdates(  provider, 5000, 0, this);
+                    }
+                }
+        }
     }
 }
